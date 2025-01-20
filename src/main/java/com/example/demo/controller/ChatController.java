8cf9a254204acfb6e168bdeb182a2948e5e574d6
sergiokiselev <sergio.kiselev509@gmail.com>
2025-01-20 19:08:12 +0100

    Update ChatController.java

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -5,21 +5,14 @@ import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.document.Document;
-import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -28,26 +21,14 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final OllamaChatModel chatModel;
+    private final OpenAiChatModel chatModel;
 
     private final VectorStore vectorStore;
 
-    private final ResourceLoader resourceLoader;
 
-    @Autowired
-    public void init() {
-        try {
-            Resource resource = resourceLoader.getResource("classpath:static/documents.txt");
-            InputStream inputStream = resource.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            List<String> lines = reader.lines().toList();
-
-            List<Document> collect = lines.stream().map(Document::new).toList();
-
-            vectorStore.add(collect);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @GetMapping("/ai/add-document")
+    public void addDocument(@RequestParam(value = "document") String document) {
+        vectorStore.add(List.of(new Document(document)));
     }
 
     @GetMapping("/ai/generate")
@@ -67,6 +48,7 @@ public class ChatController {
                         .withTopK(5))
                 .stream()
                 .map(Document::getContent)
-                .toList();
+                .collect(Collectors.toList());
+//                .toList();
     }
 }
