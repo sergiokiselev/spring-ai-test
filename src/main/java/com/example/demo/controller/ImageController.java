e21ab8f24c8900cdfbea1c5ffa7baa13c0190419
sergiokiselev <sergio.kiselev509@gmail.com>
2025-01-20 19:06:47 +0100

    Update ImageController.java

--- src/main/java/com/example/demo/controller/ImageController.java
+++ src/main/java/com/example/demo/controller/ImageController.java
@@ -1,5 +1,11 @@
 package com.example.demo.controller;
 
+import org.postgresql.shaded.com.ongres.scram.common.bouncycastle.base64.Base64;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +22,7 @@ public class ImageController {
     }
 
     @GetMapping("/image-gen")
-    public String imageGen(@RequestParam String message) {
+    public ResponseEntity<ByteArrayResource> imageGen(@RequestParam String message) {
 
         ImageOptions options = ImageOptionsBuilder.builder()
                 .withModel("dall-e-3")
@@ -26,8 +32,18 @@ public class ImageController {
 
         ImagePrompt imagePrompt = new ImagePrompt(message, options);
         ImageResponse response = imageClient.call(imagePrompt);
-        String imageUrl = response.getResult().getOutput().getUrl();
+        String encodedImage = response.getResult().getOutput().getB64Json();
+        byte[] decoded = Base64.decode(encodedImage);
 
-        return "redirect:" + imageUrl;
+        ByteArrayResource resource = new ByteArrayResource(decoded);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(resource.contentLength())
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        ContentDisposition.attachment()
+                                .filename("whatever")
+                                .build().toString())
+                .body(resource);
+//        return "redirect:" + imageUrl;
     }
 }
